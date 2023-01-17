7049aa682a71f52f1e133fc12d807d0d6ffa2e8f
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-17 13:56:10 +0300

    update base url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://etecube-assignment-server.onrender.com";
+const BASE_URL = "https://etecube-assignment-backend.onrender.com";
 
 export const COMPANY_URL = `${BASE_URL}/companies`;
 export const PRODUCT_URL = `${BASE_URL}/products`;
